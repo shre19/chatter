8cf446e58fe5549a68a2cb951195612eca516e0b
shre19 <shreyasmohite2001@gmail.com>
2022-07-28 15:54:43 +0530

    make it better

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
 });
 
 const server = app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+  console.log(`Server started on 3000`)
 );
 const io = socket(server, {
   cors: {
